47699196678d38df3ae96e77db3e0ed66c02d705
Osimxon <osimhondadaxanov2013@gmail.com>
2026-06-13 13:59:12 +0500

    homework

---
+++ js/app.js
@@ -0,0 +1,13 @@
+﻿const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
+
+setInterval(() => {
+  const now = new Date();
+  let h = now.getHours();
+  let m = now.getMinutes();
+  let s = now.getSeconds();
+  hourEl.textContent = h;
+  minuteEl.textContent = m;
+  secondEl.textContent = s;
+}, 1000);
